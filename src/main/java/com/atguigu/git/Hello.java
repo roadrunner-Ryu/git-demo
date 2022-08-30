604062440908af5a00d0ea31c102b752e4a76652
刘总 <loadrunner_ryu@163.com>
2022-08-30 21:01:59 +0800

    第二次创建

--- src/main/java/com/atguigu/git/Hello.java
+++ src/main/java/com/atguigu/git/Hello.java
@@ -5,6 +5,7 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("HelloWorld~~");
         System.out.println("111111111111");
+        System.out.println("222222222222");
     }
 
 }
